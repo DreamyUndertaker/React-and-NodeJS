19210f88dc780384201c3c375628db8a66174100
Vadim Zalogin <vadiimreg73@gmail.com>
2022-01-25 21:04:53 +0400

    first-attmeps

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>
-            привет
-          </p>
-          Learn React
-        </a>
-      </header>
+const  App = () =>{
+  return(
+    <div classname = "App">
+      <ul>
+        <li>cs</li>
+        <li>cs</li>
+        <li>cs</li>
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
